fc5f85cadbca0b2c0583ea3264543c14b1612e14
Igor Raskatov <aura.star@yandex.ru>
2022-10-26 18:41:05 +0300

    Test3

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import './styles.scss'
 import image from './1who_let.jpg'
 import { ClickCounter } from './ClickCounter'
-const maslo =  432
+const name = 'jija'
 export const App = () => {
   return (
     <>
       <h1>
-        React TypeScript Webpack Starter Template - {process.env.NODE_ENV}
+        React TypeScript Webpack Starter Template - {process.env.NODE_ENV}{' '}
+        {name}
         {process.env.name}
       </h1>
       <img src={image} width="100px" height="100px" alt="A dog take place" />
