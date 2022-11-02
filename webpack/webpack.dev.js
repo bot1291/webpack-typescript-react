83fbf0e83aea94633c6110a4a8123d53b11117f3
Igor Raskatov <aura.star@yandex.ru>
2022-11-02 16:22:32 +0300

    Fix import css.main

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -4,7 +4,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   mode: 'development',
-  devtool: 'eval-cheap-module-source-map',
+  devtool: 'source-map',
   devServer: {
     hot: true,
     open: true,
